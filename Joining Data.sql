da376282b385599fad1b26c327a0510f06346252
Tahseen Chisti <43124551+tazchisti@users.noreply.github.com>
2018-12-20 15:21:52 -0500

    Add files via upload

---
+++ Joining Data.sql
@@ -0,0 +1,8 @@
+create database restaurant_db;
+use restaurant_db;
+
+
+select*from yelp_reviews 
+left outer join yp_reviews on yelp_reviews.address = yp_reviews.address
+left outer join google_reviews on yelp_reviews.address = google_reviews.address
+group by name;
