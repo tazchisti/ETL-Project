ed1fbd9c7ec7f9f003b158de7cc71e421d0be000
Lauren Gama <laurenngama@gmail.com>
2018-12-20 21:06:00 -0500

    added files

--- Joining Data.sql
+++ Joining Data.sql
@@ -1,8 +1,7 @@
 create database restaurant_db;
 use restaurant_db;
 
-
-select*from yelp_reviews 
+select*from yelp_reviews
 left outer join yp_reviews on yelp_reviews.address = yp_reviews.address
-left outer join google_reviews on yelp_reviews.address = google_reviews.address
-group by name;
+left outer join google_reviews on yelp_reviews.address = google_reviews.address;
+
